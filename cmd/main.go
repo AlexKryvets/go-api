696cec09aacb1085f36391c0ef2dc1150eed4aa4
Alexey Kryvets <alex.kryvets@gmail.com>
2021-12-20 17:01:18 +0200

    fix: naming collision local vars and packages

--- cmd/main.go
+++ cmd/main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	repository := repository.NewRepository()
-	service := service.NewService(repository)
-	handler := handler.NewHandler(service)
+	_repository := repository.NewRepository()
+	_service := service.NewService(_repository)
+	_handler := handler.NewHandler(_service)
 
 	srv := new(go_api.Server)
-	if err := srv.Run("8000", handler.InitRouters()); err != nil {
+	if err := srv.Run("8000", _handler.InitRouters()); err != nil {
 		log.Fatalf("Error")
 	}
 }
